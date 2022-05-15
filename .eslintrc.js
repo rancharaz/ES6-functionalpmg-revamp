fb17c6c07c5f8f6385403f7b2dc90e2ab2c4b086
rancharaz <ranveer1990@hotmail.com>
2022-05-15 22:34:35 +0400

    last challenge validation form type

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-/* module.exports = {
+module.exports = {
   env: {
     browser: true,
     es2021: true,
@@ -6,14 +6,13 @@
   extends: [
     /*  'airbnb-base', */
   ],
-parserOptions: {
-  ecmaVersion: 'latest',
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
-plugins: [
-  'immutable',
-],
+  plugins: [
+    'immutable',
+  ],
   rules: {
-  'immuatable/no-mutable': 2,
+    'immuatable/no-mutable': 2,
   },
 };
- * /
